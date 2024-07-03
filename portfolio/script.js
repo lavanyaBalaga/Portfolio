dbc808a365cc6e0a78bd565b178f46a88d7d0261
lavanyaBalaga <95291752+lavanyaBalaga@users.noreply.github.com>
2024-07-03 16:53:03 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,16 @@
+function portfolio() {
+    window.location.href = "https://github.com/lavanyaBalaga/Portfolio";
+}
+function emailspam(){
+    window.location.href="https://github.com/lavanyaBalaga/Email-Spam-Project"
+}
+function UIE(){
+    window.location.href="https://github.com/lavanyaBalaga/UIE_MLLE"
+}
+function hackerrank() {
+    window.location.href = "https://github.com/lavanyaBalaga/HackerRank-Clone";
+}
+var typed = new Typed('#element', {
+    strings: ['Student', 'Developer', 'Programming Enthusiast'],
+    typeSpeed: 40,
+});
